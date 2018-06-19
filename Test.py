daa8092ff18c61e16bb3162934ba272f9047ab15
dreezyd <40373376+dreezyd@users.noreply.github.com>
2018-06-18 17:44:20 -0700

    Test on Jupyter

---
+++ Test.py
@@ -0,0 +1,60 @@
+
+# coding: utf-8
+
+# In[9]:
+
+car_cost = 20000
+car_tax = 3000
+
+total_cost = car_cost + car_tax
+
+
+# In[2]:
+
+print(car_cost)
+
+
+# In[3]:
+
+print(car_tax)
+
+
+# In[4]:
+
+print(total_cost)
+
+
+# In[5]:
+
+print(car_tax)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[6]:
+
+car_tax
+
+
+# In[10]:
+
+final_cost_with_rebate = total_cost * 0.8 
+
+
+# In[11]:
+
+final_cost_with_rebate
+
+
+# In[ ]:
+
+
+
